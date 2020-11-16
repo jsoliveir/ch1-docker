1808834a0e1055ec200e7ba9af64e0bd1a84b116
José Oliveira <jose.oliveira@sbanken.no>
2020-11-16 19:59:10 +0000

    added testing controller

---
+++ microservices/api.core.mail/src/Controllers/MailController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Core.Mail.Models;
+using Api.Core.Mail.Observers;
+using Api.Core.Mail.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Core.Mail.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[Controller]")]
+    public class MailController : Controller
+    {
+        private readonly IMailService _emailService;
+        public MailController(IMailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Send([FromBody]Email email)
+        {
+            await _emailService.SendMail(email);
+            return Ok("mail sent");
+        }
+    }
+}

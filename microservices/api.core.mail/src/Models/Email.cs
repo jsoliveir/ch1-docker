d8d16d44981620d2458ec258fb37da712937bcef
José Oliveira <jose.oliveira@sbanken.no>
2020-11-16 19:58:58 +0000

    added Email model

---
+++ microservices/api.core.mail/src/Models/Email.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Core.Mail.Models
+{
+    public class Email
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}

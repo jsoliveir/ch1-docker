fe0e1a7f1b7dfd8b216259d7f8b6e9181eb287b2
José Oliveira <jose.oliveira@sbanken.no>
2020-11-17 19:38:56 +0000

    added service controllers (api.core.subscriptions)

---
+++ microservices/api.core.subscriptions/src/Controllers/v1/ISubscriptions.cs
@@ -0,0 +1,15 @@
+﻿using Api.Core.Subscriptions.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Api.Core.Subscriptions.Controllers.v1
+{
+    public interface ISubscriptions
+    {
+        ActionResult<Subscription> Create(Subscription subscription);
+        ActionResult<Subscription> Get(int id);
+        ActionResult<IEnumerable<Subscription>> List(int max = 10);
+        ActionResult Delete(int id);
+        ActionResult<Subscription> Put(int id, Subscription subscription);
+    }
+}
---
+++ microservices/api.core.subscriptions/src/Controllers/v1/SubscriptionController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Api.Core.Subscriptions.Controllers;
+using Api.Core.Subscriptions.Models;
+using Api.Core.Subscriptions.Services;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Core.Subscriptions.Controllers.v1
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class SubscriptionController : Controller, ISubscriptions
+    {
+        private readonly ILogger<SubscriptionController> _logger;
+        private readonly ICoreEmailService _mailService;
+        private readonly ISubscriptionsDbService _subscriptionsService;
+
+        public SubscriptionController(
+            ISubscriptionsDbService subscriptionsService,
+            ILogger<SubscriptionController> logger,
+            ICoreEmailService mailService)
+        {
+            _logger = logger;
+            _mailService = mailService;
+            _subscriptionsService = subscriptionsService;
+        }
+
+        [HttpPost]
+        public ActionResult<Subscription> Create(Subscription subscription)
+        {
+            try
+            {
+                subscription = _subscriptionsService.CreateSubscription(subscription);
+                _mailService.Send(new Email()
+                {
+                    Subject = "Subcription Created",
+                    Body = $"Tank you for subscribe <br/> " +
+                        $"{JsonSerializer.Serialize(subscription)}",
+                    From = "no-reply@iban.com",
+                    To = subscription.Email,
+                });
+
+                return Ok(subscription);
+            }
+            catch (Exception)
+            {
+                _logger.LogError("Error while attempting to create a subscription");
+            }
+
+            return Forbid("Error while attempting to create a subscription");
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete([FromRoute] int id)
+        {
+            try
+            {
+                var subscription = _subscriptionsService.GetSubscriptionById(id);
+
+                if (subscription?.Id == null)
+                    return NotFound();
+
+                _subscriptionsService.DeleteSubscription(subscription);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                return BadRequest(ex.Message);
+            }
+
+            return Ok("OK");
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Subscription> Put([FromRoute] int id, [FromBody] Subscription subscription)
+        {
+            var origin = _subscriptionsService.GetSubscriptionById(id);
+
+            if (origin?.Id == null)
+                return NotFound();
+
+            subscription.Id = origin.Id;
+            origin = _subscriptionsService.UpdateSubscription(subscription);
+           
+            return Ok(origin);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Subscription> Get([FromRoute] int id)
+        {
+
+            var subscription = _subscriptionsService.GetSubscriptionById(id);
+
+            if (subscription?.Id == null)
+                return NotFound();
+
+            return Ok(subscription);
+        }
+
+        [HttpGet("List")]
+        public ActionResult<IEnumerable<Subscription>> List([FromQuery] int max = 10)
+        {
+            var subscriptions = _subscriptionsService.GetSubscriptions(max);
+            return Ok(subscriptions);
+        }
+    }
+}

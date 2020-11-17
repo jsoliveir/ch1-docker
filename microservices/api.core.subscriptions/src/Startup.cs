2f175f177fa3623f6c78593f7e41035a41031afb
José Oliveira <jose.oliveira@sbanken.no>
2020-11-17 19:40:06 +0000

    added DI and configured entity framework for supporting in-memory databases (api.core.subscriptions)

--- microservices/api.core.subscriptions/src/Startup.cs
+++ microservices/api.core.subscriptions/src/Startup.cs
@@ -3,30 +3,42 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Api.Core.Subscriptions.Configurations;
+using Api.Core.Subscriptions.Databases;
+using Api.Core.Subscriptions.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client.Core.DependencyInjection;
+using RabbitMQ.Client.Core.DependencyInjection.Configuration;
 
 namespace Api.Core.Subscriptions
 {
     public class Startup
     {
-         public Startup(IWebHostEnvironment env)
+        public Startup(IWebHostEnvironment env)
         {
             Configuration = new ConfigurationBuilder()
-               .AddJsonFile("appsettings.json",false)
-               .AddJsonFile($"appsettings.{env.EnvironmentName}.json",true)
+               .AddJsonFile("appsettings.json", false)
+               .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
                .AddEnvironmentVariables()
                .Build();
 
+            SeqConfiguration = new SeqConfiguration();
+            RabbitMQConfiguration = new RabbitMQConfiguration();
+            Configuration.GetSection("RabbitMQ").Bind(RabbitMQConfiguration);
+            Configuration.GetSection("Seq").Bind(SeqConfiguration);
         }
 
         public IConfigurationRoot Configuration { get; }
+        public SeqConfiguration SeqConfiguration { get; }
+        public RabbitMQConfiguration RabbitMQConfiguration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
@@ -34,7 +46,7 @@ namespace Api.Core.Subscriptions
 
             services.AddControllers();
 
-            services.AddSwaggerGen(c=>
+            services.AddSwaggerGen(c =>
             {
                 var xmlFile = $"documentation.xml";
                 var xmlPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, xmlFile);
@@ -42,12 +54,26 @@ namespace Api.Core.Subscriptions
             });
 
             services.AddLogging(loggingBuilder =>
-            {                
+            {
                 loggingBuilder
-                .SetMinimumLevel(Enum.Parse<LogLevel>(Configuration["Seq:MinimumLevel"]))
-                .AddSeq(Configuration["Seq:ServerUrl"]);
+                .SetMinimumLevel(SeqConfiguration.MinimumLevel)
+                .AddSeq(SeqConfiguration.ServerUrl);
             });
 
+            services.AddSingleton(SeqConfiguration);
+            services.AddSingleton(RabbitMQConfiguration);
+
+            services.AddScoped<ICoreEmailService, CoreEmailService>();
+            services.AddScoped<ISubscriptionsDbService, SubscriptionDbService>();
+
+            services.AddDbContext<SubscriptionsDb>(
+              options => options.UseInMemoryDatabase(databaseName: "Subscriptions"));
+
+            services.AddRabbitMqProducingClientSingleton(RabbitMQConfiguration.Client);
+
+            services.AddProductionExchange(
+                RabbitMQConfiguration.ExchangeName,
+                RabbitMQConfiguration.Exchange);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -64,11 +90,7 @@ namespace Api.Core.Subscriptions
                 c.SwaggerEndpoint("v1/swagger.json", this.GetType().Namespace);
             });
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {

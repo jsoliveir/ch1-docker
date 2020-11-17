f78be999838ee76cd88ed0c66de7478aeaa55bac
José Oliveira <jose.oliveira@sbanken.no>
2020-11-17 19:44:51 +0000

    added authentication layer (tokens not validated, use any string) (api.client.subscriptions)

---
+++ microservices/api.client.subscriptions/src/Authentication/ApiKeyAuthenticationHandler.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace Api.Client.Subscriptions.Authentication
+{
+    public class ApiKeyAuthenticationHandler : AuthenticationHandler<ApiKeyAuthenticationOptions>
+    {
+        private const string ProblemDetailsContentType = "application/problem+json";
+        private const string ApiKeyHeaderName = "authorization";
+
+        public ApiKeyAuthenticationHandler(
+            IOptionsMonitor<ApiKeyAuthenticationOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.TryGetValue(ApiKeyHeaderName, out var apiKeyHeaderValues))
+                return Task.FromResult(AuthenticateResult.NoResult());
+
+            var providedApiKey = apiKeyHeaderValues.FirstOrDefault();
+
+            if (apiKeyHeaderValues.Count == 0 || string.IsNullOrWhiteSpace(providedApiKey))
+                return Task.FromResult(AuthenticateResult.Fail("Invalid API Key provided."));
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, Assembly.GetEntryAssembly().GetName().Name)
+            };
+
+            var identity = new ClaimsIdentity(claims, Options.AuthenticationType);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Options.Scheme);
+            return Task.FromResult(AuthenticateResult.Success(ticket));
+        }
+    
+    }
+}

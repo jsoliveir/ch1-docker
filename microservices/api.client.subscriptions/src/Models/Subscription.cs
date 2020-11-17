4a2d94a1aeff6096fd5ba413b517e0a48bcb11ef
José Oliveira <jose.oliveira@sbanken.no>
2020-11-17 19:41:44 +0000

    added models (api.client.subscriptions)

---
+++ microservices/api.client.subscriptions/src/Models/Subscription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Api.Core.Subscriptions.Models
+{
+    public class Subscription : SubscriptionViewModel
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ microservices/api.client.subscriptions/src/Models/SubscriptionViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Api.Core.Subscriptions.Models
+{
+    public class SubscriptionViewModel
+    {
+        [MaxLength(20, ErrorMessage = "Not a valid name")]
+        public string FirstName { get; set; }
+
+        public Gender Gender { get; set; } = Gender.NA;
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Not a valid email")]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public bool Consent { get; set; }
+
+        [Required]
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "Not a valid id")]
+        public int NewsletterId { get; set; }
+
+    }
+}

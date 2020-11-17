29d4046740de1d1931136c4a0183c390c7fa2cc4
José Oliveira <jose.oliveira@sbanken.no>
2020-11-17 19:36:53 +0000

    added service configurations (api.core.subscriptions)

---
+++ microservices/api.core.subscriptions/src/Configurations/RabbitMQConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+using RabbitMQ.Client.Core.DependencyInjection.Configuration;
+
+namespace Api.Core.Subscriptions.Configurations
+{
+    public class RabbitMQConfiguration
+    {
+        public string ExchangeName { get; set; }
+
+        public RabbitMqClientOptions Client { get; set; }
+
+        public RabbitMqExchangeOptions Exchange { get; set; }
+    }
+
+}
---
+++ microservices/api.core.subscriptions/src/Configurations/SeqConfiguration.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.Extensions.Logging;
+
+namespace Api.Core.Subscriptions.Configurations
+{
+    public class SeqConfiguration
+    { 
+         public string ServerUrl { get; set; }
+
+         public LogLevel MinimumLevel { get; set; }
+    }
+
+}

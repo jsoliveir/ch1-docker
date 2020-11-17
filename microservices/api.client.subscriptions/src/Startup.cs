f64f367443c3ef7e84cd5beacaa54fe07e62ce7b
José Oliveira <jose.oliveira@sbanken.no>
2020-11-17 19:41:29 +0000

    added configurations (api.client.subscriptions)

--- microservices/api.client.subscriptions/src/Startup.cs
+++ microservices/api.client.subscriptions/src/Startup.cs
@@ -2,8 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Reflection;
+using System.Reflection.Metadata;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Api.Client.Subscriptions.Authentication;
+using Api.Client.Subscriptions.Middlewares;
+using Api.Core.Subscriptions.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +18,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Api.Client.Subscriptions
 {
@@ -20,8 +27,8 @@ namespace Api.Client.Subscriptions
         public Startup(IWebHostEnvironment env)
         {
             Configuration = new ConfigurationBuilder()
-               .AddJsonFile("appsettings.json",false)
-               .AddJsonFile($"appsettings.{env.EnvironmentName}.json",true)
+               .AddJsonFile("appsettings.json", false)
+               .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
                .AddEnvironmentVariables()
                .Build();
 
@@ -31,24 +38,63 @@ namespace Api.Client.Subscriptions
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOptions();
+            services.AddControllers()
+                .AddJsonOptions(opts =>
+                {
+                    opts.JsonSerializerOptions.Converters.Add(
+                        new JsonStringEnumConverter());
+                });
 
-            services.AddControllers();
-
-            services.AddSwaggerGen(c=>
+            services.AddSwaggerGen(c =>
             {
                 var xmlFile = $"documentation.xml";
                 var xmlPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Api bearer Token",
+                    Scheme = "Bearer",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
             });
 
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = ApiKeyAuthenticationOptions.DefaultScheme;
+                options.DefaultChallengeScheme = ApiKeyAuthenticationOptions.DefaultScheme;
+            })
+            .AddScheme<ApiKeyAuthenticationOptions, ApiKeyAuthenticationHandler>(
+                ApiKeyAuthenticationOptions.DefaultScheme, (o) => { });
+
             services.AddLogging(loggingBuilder =>
-            {                
+            {
                 loggingBuilder
                 .SetMinimumLevel(Enum.Parse<LogLevel>(Configuration["Seq:MinimumLevel"]))
                 .AddSeq(Configuration["Seq:ServerUrl"]);
             });
 
+            services.AddHttpClient<ICoreSubscriptionsService, CoreSubscriptionsService>(client =>
+            {
+                client.BaseAddress = new Uri(Configuration["Api:CoreSubscriptions:BaseAddress"]);
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -58,16 +104,33 @@ namespace Api.Client.Subscriptions
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseSwagger();
+            app.UseSwagger(c=>
+            {
+                c.PreSerializeFilters.Add((document, request) =>
+                {
+                    if (request.Headers.TryGetValue("X-Forwarded-Path",out var path))
+                    {
+                        document.Servers = new List<OpenApiServer>
+                        {
+                            new OpenApiServer()
+                            { 
+                                Url = $"{path}" 
+                            }
+                        };
+                    }
+                });
+            });
+
+            app.UseMiddleware<HttpExceptionMiddleware>();
 
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("v1/swagger.json", this.GetType().Namespace);
             });
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
@@ -75,6 +138,7 @@ namespace Api.Client.Subscriptions
             {
                 endpoints.MapControllers();
             });
+
         }
     }
 }

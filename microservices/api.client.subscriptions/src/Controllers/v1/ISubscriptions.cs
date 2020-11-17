928704de95bf45261641a0eb97fe13865014a77a
José Oliveira <jose.oliveira@sbanken.no>
2020-11-17 19:45:21 +0000

    added service controllers (api.client.subscriptions)

---
+++ microservices/api.client.subscriptions/src/Controllers/v1/ISubscriptions.cs
@@ -0,0 +1,15 @@
+﻿using Api.Core.Subscriptions.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Client.Subscriptions.Controllers.v1
+{
+    public interface ISubscriptions
+    {
+        Task<ActionResult> Delete(int id);
+        Task<ActionResult<Subscription>> Create(SubscriptionViewModel subscription);
+        Task<ActionResult<Subscription>> Get([FromRoute] int id);
+        Task<ActionResult<IEnumerable<Subscription>>> List([FromQuery] int max = 10);
+    }
+}
---
+++ microservices/api.client.subscriptions/src/Controllers/v1/SubscriptionController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Client.Subscriptions.Controllers.v1;
+using Api.Core.Subscriptions.Models;
+using Api.Core.Subscriptions.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Core.Subscriptions.Controllers.v1
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class SubscriptionController : Controller, ISubscriptions
+    {
+        private readonly ILogger<SubscriptionController> _logger;
+        private readonly ICoreSubscriptionsService _coreSubscriptionsService;
+
+        public SubscriptionController(
+            ICoreSubscriptionsService coreSubscriptionsService,
+            ILogger<SubscriptionController> logger)
+        {
+            _logger = logger;
+            _coreSubscriptionsService = coreSubscriptionsService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Subscription>> Create(SubscriptionViewModel subscription)
+        {
+            try
+            {
+                subscription =  await _coreSubscriptionsService.Create(subscription);
+                return Ok(subscription);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete([FromRoute] int id)
+        {
+            await _coreSubscriptionsService.Delete(id);
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Subscription>> Get([FromRoute] int id)
+        {
+            var subscription = await _coreSubscriptionsService.Get(id);
+            return Ok(subscription);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<ActionResult<Subscription>> Patch([FromRoute] int id,[FromQuery] bool? consent)
+        {
+            var subscription = await _coreSubscriptionsService.Get(id);
+
+            subscription.Consent = 
+                consent ?? subscription.Consent;
+
+            await _coreSubscriptionsService.Update(id,subscription);
+            return Ok(subscription);
+        }
+
+        [HttpGet("List")]
+        public async Task<ActionResult<IEnumerable<Subscription>>> List([FromQuery] int max=10)
+        {
+            var subscriptions = await _coreSubscriptionsService.List(max);
+            return Ok(subscriptions);
+        }
+    }
+}
